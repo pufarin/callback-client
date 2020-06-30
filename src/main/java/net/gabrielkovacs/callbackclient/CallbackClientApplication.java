f5ba1e3b54db1b851b1ba722112e0c5e721995e9
gabrielk <mail@gabrielkovacs.net>
2020-06-30 10:33:51 +0200

    first commit

---
+++ src/main/java/net/gabrielkovacs/callbackclient/CallbackClientApplication.java
@@ -0,0 +1,13 @@
+package net.gabrielkovacs.callbackclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallbackClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallbackClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/gabrielkovacs/callbackclient/CallbackClientApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gabrielkovacs.callbackclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallbackClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
